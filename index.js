a1b09ce0dac23003d12e89193919eae731b0dcb9
md-moynul <mdmoynulislam58995@gmail.com>
2026-06-23 13:07:11 +0600

    add ObjectId import and implement recipe retrieval and update endpoints

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const cors = require('cors');
 
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
@@ -24,12 +24,12 @@ app.get('/', (req, res) => {
 })
 async function run() {
     try {
-        
+
         // await client.connect();
         const db = client.db("recipely-db");
         const usersCollection = db.collection("user");
         const recipesCollections = db.collection('recipes')
-        
+
         // user related api 
         app.get('/api/users', async (req, res) => {
             const users = await usersCollection.find().toArray();
@@ -43,13 +43,28 @@ async function run() {
             res.send(newRecipe);
         });
         app.get('/api/my-recipe', async (req, res) => {
-            let query ={};
-            if(req.query.authorId){
+            let query = {};
+            if (req.query.authorId) {
                 query.authorId = req.query.authorId
             }
             const cursor = recipesCollections.find(query);
             const recipes = await cursor.toArray();
             res.send(recipes);
+        })
+        app.get('/api/my-recipe/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+
+            const query = { _id: new ObjectId(id) };
+            const recipe = await recipesCollections.findOne(query);
+            res.send(recipe);
+        })
+        app.patch('/api/my-recipe/:id', async (req, res) => {
+            const id = req.params.id;
+            const recipe = req.body;
+            const query = { _id: new ObjectId(id) };
+            const updateRecipe = await recipesCollections.updateOne(query, { $set: recipe });
+            res.send(updateRecipe);
         })
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
